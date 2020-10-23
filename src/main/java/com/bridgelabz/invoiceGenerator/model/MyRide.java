5f661e18dede9cbb480df566f2f82d64d2857625
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-23 23:28:46 +0530

    Add: MyRide def class for Ride obj

---
+++ src/main/java/com/bridgelabz/invoiceGenerator/model/MyRide.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.invoiceGenerator.model;
+
+public class MyRide {
+
+	public double distance;
+	public int time;
+	
+	public MyRide(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+	
+	
+}
