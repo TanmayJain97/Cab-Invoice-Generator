37cc044e86f765fad78c5a59bf6beb6e95cf8339
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-24 08:27:29 +0530

    Refactor: Rides now support multiple types

--- src/main/java/com/bridgelabz/invoiceGenerator/model/MyRide.java
+++ src/main/java/com/bridgelabz/invoiceGenerator/model/MyRide.java
@@ -2,13 +2,14 @@ package com.bridgelabz.invoiceGenerator.model;
 
 public class MyRide {
 
+	RideTypes type;
 	public double distance;
 	public int time;
+	public RideTypes cabRide;
 	
-	public MyRide(double distance, int time) {
+	public MyRide(RideTypes cabRide,double distance, int time) {
+		this.cabRide=cabRide;
 		this.distance = distance;
 		this.time = time;
 	}
-	
-	
 }
